0d154001728e947c51590dbf7c042c23114b47d8
Nina Jovanovik <ninajovanovik@yahoo.com>
2022-06-02 21:51:37 +0200

    baranje 4 odgovoreno.

--- SILab2.java
+++ SILab2.java
@@ -4,38 +4,40 @@ import java.util.List;
 public class SILab2 {
 
     public static List<String> function(List<String> list) {
-        if (list.size() <= 0) {
-            throw new IllegalArgumentException("List length should be greater than 0");
+	// 0 - vlez vo funkcija
+        if (list.size() <= 0) { // 1
+            throw new IllegalArgumentException("List length should be greater than 0"); // 2
         }
-        int n = list.size();
-        int rootOfN = (int) Math.sqrt(n);
-        if (rootOfN * rootOfN  != n) {
-            throw new IllegalArgumentException("List length should be a perfect square");
+        int n = list.size(); // 3
+        int rootOfN = (int) Math.sqrt(n); // 4
+        if (rootOfN * rootOfN  != n) { // 5
+            throw new IllegalArgumentException("List length should be a perfect square"); // 6
         }
-        List<String> numMines = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (!list.get(i).equals("#")) {
-                int num = 0;
-                if ( (i % rootOfN != 0 && list.get(i - 1).equals("#")) || (i % rootOfN != rootOfN - 1 && list.get(i + 1).equals("#")) ) {
-                    if ( (i % rootOfN != 0 && list.get(i - 1).equals("#")) && (i % rootOfN != rootOfN - 1 && list.get(i + 1).equals("#")) ){
-                        num += 2;
+        List<String> numMines = new ArrayList<>(); // 7
+        for (int i = 0; i < n; i++) { // 8
+            if (!list.get(i).equals("#")) { // 9
+                int num = 0; // 10
+                if ( (i % rootOfN != 0 && list.get(i - 1).equals("#")) || (i % rootOfN != rootOfN - 1 && list.get(i + 1).equals("#")) ) { // 11
+                    if ( (i % rootOfN != 0 && list.get(i - 1).equals("#")) && (i % rootOfN != rootOfN - 1 && list.get(i + 1).equals("#")) ){ // 12
+                        num += 2; // 13
                     }
-                    else {
-                        num  += 1;
+                    else { // 14
+                        num  += 1; // 15
                     }
                 }
-                if (i - rootOfN >= 0 && list.get(i - rootOfN).equals("#")){
-                    num++;
+                if (i - rootOfN >= 0 && list.get(i - rootOfN).equals("#")){ // 16
+                    num++; // 17
                 }
-                if (i + rootOfN < n && list.get(i + rootOfN).equals("#")){
-                    num++;
+                if (i + rootOfN < n && list.get(i + rootOfN).equals("#")){ // 18
+                    num++; // 19
                 }
-                numMines.add(String.valueOf(num));
+                numMines.add(String.valueOf(num)); // 20
             }
-            else {
-                numMines.add(list.get(i));
+            else { // 21
+                numMines.add(list.get(i)); // 22
             }
         }
-        return numMines;
+        return numMines; // 23
     }
+	// 24 - izlez od funkcija
 }
